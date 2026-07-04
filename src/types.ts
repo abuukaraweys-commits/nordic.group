ed7bfe9fefcf37f3b14be868483df80335a779dc
abuukaraweys-commits <abuukaraweys@gmail.com>
2026-07-04 19:58:27 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ export interface Product {
   name: string;
   category: 'composites' | 'tools' | 'imaging';
   description: string;
+  indications?: string;
   features: string[];
   specifications: Record<string, string>;
   imageUrl?: string;
